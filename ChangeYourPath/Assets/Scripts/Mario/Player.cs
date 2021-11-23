c779e7d1d06b52e351b7968957a5a50b9f8483dc
LorisPanza <72934724+LorisPanza@users.noreply.github.com>
2021-11-23 18:11:08 +0100

    Push movimento

--- ChangeYourPath/Assets/Scripts/Mario/Player.cs
+++ ChangeYourPath/Assets/Scripts/Mario/Player.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private CapsuleCollider2D boxCollider;
+    private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
     public int velocity=1;
     private RaycastHit2D hit;
     public Animator animator;
+    public GameObject movePoint;
 
     //added comment
 
     // Start is called before the first frame update
     void Start()
     {
-        boxCollider = GetComponent<CapsuleCollider2D>();
+        boxCollider = movePoint.GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate()
@@ -31,13 +32,13 @@ public class Player : MonoBehaviour
         animator.SetFloat("Vertical", y);
         animator.SetFloat("Speed", moveDelta.sqrMagnitude);
 
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0,
+        hit = Physics2D.BoxCast(movePoint.transform.position, boxCollider.size, 0,
             new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y*Time.deltaTime*velocity),LayerMask.GetMask("Player", "Obstacle"));
         
         if(hit.collider == null) 
             transform.Translate(0,moveDelta.y*Time.deltaTime*velocity,0);
         
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0,
+        hit = Physics2D.BoxCast(movePoint.transform.position, boxCollider.size, 0,
             new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x*Time.deltaTime*velocity),LayerMask.GetMask("Player", "Obstacle"));
         
         if(hit.collider == null)
