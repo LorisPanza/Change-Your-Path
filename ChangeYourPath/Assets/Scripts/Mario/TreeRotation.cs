658ebd1ae3d6b2eafe0d18e36f40947d034f3d2d
rocamario <72744822+rocamario@users.noreply.github.com>
2021-11-26 19:40:03 +0100

    Rotation problem

---
+++ ChangeYourPath/Assets/Scripts/Mario/TreeRotation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeRotation : MonoBehaviour
+{
+    public GameObject forest0;
+    public GameObject forest90;
+    public GameObject forest180;
+    public GameObject forest270;
+}
