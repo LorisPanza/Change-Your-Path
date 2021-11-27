c79ae913f48aa4dd276865f38c503ad45341baee
rocamario <72744822+rocamario@users.noreply.github.com>
2021-11-27 11:58:05 +0100

    snow ocean piece

--- ChangeYourPath/Assets/Scripts/Mario/TreeRotation.cs
+++ ChangeYourPath/Assets/Scripts/Mario/TreeRotation.cs
@@ -18,10 +18,34 @@ public class TreeRotation : MonoBehaviour
         forest270.SetActive(false);
 
         //Debug.Log(name + ":  " + transform.rotation.z);
-        if (transform.rotation.eulerAngles.z == 0) forest0.SetActive(true);
-        else if (transform.rotation.eulerAngles.z == 90)forest90.SetActive(true);
-        else if (transform.rotation.eulerAngles.z == 180) forest180.SetActive(true);
-        else if (transform.rotation.eulerAngles.z == -90) forest270.SetActive(true);
+        if (transform.rotation.eulerAngles.z == 0)
+        {
+            forest0.SetActive(true);
+            InitTrees(0);
+        }
+        else if (transform.rotation.eulerAngles.z == 90)
+        {
+            forest90.SetActive(true);
+            InitTrees(90);
+        }
+        else if (transform.rotation.eulerAngles.z == 180)
+        {
+            forest180.SetActive(true);
+            InitTrees(180);
+        }
+        else if (transform.rotation.eulerAngles.z == 270)
+        {
+            forest270.SetActive(true);
+            InitTrees(90);
+        }
+        Debug.Log(name + ":  " + transform.rotation.eulerAngles.z);
+    }
 
+    private void InitTrees(int degree)
+    {
+        forest0.transform.rotation = Quaternion.Euler(0, 0, degree);
+        forest90.transform.rotation = Quaternion.Euler(0, 0, degree - 90);
+        forest180.transform.rotation = Quaternion.Euler(0, 0, degree - 180);
+        forest270.transform.rotation = Quaternion.Euler(0, 0, degree + 90);
     }
 }
