c14a8be4da9f76d56801713231bee13b3f6ef9bc
rocamario <72744822+rocamario@users.noreply.github.com>
2021-11-28 16:21:45 +0100

    cespugli

---
+++ ChangeYourPath/Assets/Scripts/Mario/Bushes.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bushes : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent.gameObject.GetComponent<SpriteRenderer>().sortingOrder =
+                this.GetComponent<SpriteRenderer>().sortingOrder - 1;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent.gameObject.GetComponent<SpriteRenderer>().sortingOrder =
+                this.GetComponent<SpriteRenderer>().sortingOrder + 1;
+        }
+    }
+}
