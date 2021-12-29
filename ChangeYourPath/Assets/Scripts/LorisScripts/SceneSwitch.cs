b79ec08d2e51a2a805f6562aa20db69dee27f63f
LorisPanza <72934724+LorisPanza@users.noreply.github.com>
2021-12-29 16:37:45 +0100

    p

--- ChangeYourPath/Assets/Scripts/LorisScripts/SceneSwitch.cs
+++ ChangeYourPath/Assets/Scripts/LorisScripts/SceneSwitch.cs
@@ -1,18 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class SceneSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public void OnTriggerEnter2D(Collider2D other)
+   {
+      if (other.CompareTag("Player"))
+      {
+         SceneManager.LoadScene("SpringScene");
+      }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   }
 }
